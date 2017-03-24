7f2e1c7ac141b5f053fe062d155af74ddbdee592
mshibli786 <mshibli786@gmail.com>
2017-03-24 18:17:49 +0530

    router added

--- Forms.jsx
+++ Forms.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 //import { Router, Route, browserHistory, IndexRoute  } from 'react-router'
 
+import { BrowserRouter as Router,  Route,Link } from 'react-router-dom';
 
 export class App extends React.Component{
 
 render(){
 return (
 <div>
+<Router>
+    <div>
+      <ul>
+        <li><Link to="/">App</Link></li>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
 
-	<ul>
-		<li>Home</li>
-		<li>About</li>
-		<li>Contact</li>
-	</ul>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+
+     <Route path="/home/:id" component={Home}/>
+
+    </div>
+  </Router>
 	
  {this.props.children}
 
@@ -64,15 +77,4 @@ render(){
 }
 }
 
-ReactDOM.render((
-   <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         <IndexRoute component = {Home} />
-         <Route path = "home" component = {Home} />
-         <Route path = "about" component = {About} />
-         <Route path = "contact" component = {Contact} />
-      </Route>
-   </Router>
-	
-), document.getElementById('app'))
 
--- main.js
+++ main.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App.jsx';
 //import Test from './Test.jsx';
-import Validation from './Validation.jsx';
-import ComponentApi from './Component.jsx';
-// import {App,Home,About,Contact} from './Forms.jsx';
-//import { Router, Route, browserHistory  } from 'react-router'
+// import Validation from './Validation.jsx';
+// import ComponentApi from './Component.jsx';
+
+
+ import {App,Home,Contact,About} from './Forms.jsx';
 
 import {AddTodoForm ,TodoList,Status} from './Redux.jsx';
 
-//import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+
+import { BrowserRouter as Router,  Route,Link } from 'react-router-dom';
+
 
 
 //ReactDOM.render(<Test />,document.getElementById('app'));
@@ -18,27 +21,4 @@ import {AddTodoForm ,TodoList,Status} from './Redux.jsx';
 
 //ReactDOM.render(<ComponentApi />,document.getElementById('val'));
 
-/*
-ReactDOM.render(
-   <Router>
-      <Route path = "/" component = {App}>
-         <Route path = "home" component = {Home} />
-         <Route path = "about" component = {About} />
-         <Route path = "contact" component = {Contact} />
-      </Route>
-   </Router>
-   
-, document.getElementById('app'));*/
-
-ReactDOM.render(
-	<div>
-	<h1>Todo</h1>
-	<AddTodoForm />
-	<Status/>
-	<TodoList />
-	</div>
-	,document.getElementById('app')
-
-
-
-	);
+ReactDOM.render(<App />,document.getElementById('app'));
